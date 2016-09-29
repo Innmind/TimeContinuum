91df782c2d91259b8fe4a170902cd6a780105019
Baptiste Langlade <baptouuuu@gmail.com>
2016-09-29 07:36:39 +0200

    add timezones for africa cities

---
+++ src/Timezone/Africa/SaoTome.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\TimeContinuum\Timezone\Africa;
+
+use Innmind\TimeContinuum\TimezoneInterface;
+
+final class SaoTome implements TimezoneInterface
+{
+    private $utc;
+
+    public function __construct()
+    {
+        $this->utc = new UTC(0, 0);
+    }
+
+    public function hours(): int
+    {
+        return $this->utc->hours();
+    }
+
+    public function minutes(): int
+    {
+        return $this->utc->minutes();
+    }
+
+    public function __toString(): string
+    {
+        return (string) $this->utc;
+    }
+}
---
+++ src/Timezone/Africa/Windhoek.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\TimeContinuum\Timezone\Africa;
+
+use Innmind\TimeContinuum\TimezoneInterface;
+
+final class Windhoek implements TimezoneInterface
+{
+    private $utc;
+
+    public function __construct()
+    {
+        $this->utc = new UTC(1, 0);
+    }
+
+    public static function summerTime(): self
+    {
+        $self = new self;
+        $self->utc = new UTC(2, 0);
+
+        return $self;
+    }
+
+    public function hours(): int
+    {
+        return $this->utc->hours();
+    }
+
+    public function minutes(): int
+    {
+        return $this->utc->minutes();
+    }
+
+    public function __toString(): string
+    {
+        return (string) $this->utc;
+    }
+}
