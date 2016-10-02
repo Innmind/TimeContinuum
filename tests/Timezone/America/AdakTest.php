ce69ddc36de1b23fbb13c3c73a5321c7527f70ac
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-02 16:27:25 +0200

    add missing UT

---
+++ tests/Timezone/America/AdakTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\TimeContinuum\Timezone\America;
+
+use Innmind\TimeContinuum\{
+    Timezone\America\Adak,
+    TimezoneInterface
+};
+
+class AdakTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $zone = new Adak;
+
+        $this->assertInstanceOf(TimezoneInterface::class, $zone);
+        if ($zone->daylightSavingTimeApplied()) {
+            $this->assertSame(-9, $zone->hours());
+            $this->assertSame(0, $zone->minutes());
+            $this->assertSame('-09:00', (string) $zone);
+        } else {
+            $this->assertSame(-10, $zone->hours());
+            $this->assertSame(0, $zone->minutes());
+            $this->assertSame('-10:00', (string) $zone);
+        }
+    }
+}
