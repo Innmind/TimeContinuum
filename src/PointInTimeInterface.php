75110f647b9d8779c94fed844e5666fbcfb2b9cd
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-05 07:50:01 +0200

    return object instead of integers for each point in time component

--- src/PointInTimeInterface.php
+++ src/PointInTimeInterface.php
@@ -3,19 +3,29 @@ declare(strict_types = 1);
 
 namespace Innmind\TimeContinuum;
 
+use Innmind\TimeContinuum\TimeContinuum\{
+    YearInterface,
+    MonthInterface,
+    DayInterface,
+    HourInterface,
+    MinuteInterface,
+    SecondInterface,
+    MillisecondInterface
+};
+
 interface PointInTimeInterface
 {
     /**
      * Since 1970-01-01T00:00:00+00:00
      */
     public function milliseconds(): int;
-    public function year(): int;
-    public function month(): int;
-    public function day(): int;
-    public function hour(): int;
-    public function minute(): int;
-    public function second(): int;
-    public function millisecond(): int;
+    public function year(): YearInterface;
+    public function month(): MonthInterface;
+    public function day(): DayInterface;
+    public function hour(): HourInterface;
+    public function minute(): MinuteInterface;
+    public function second(): SecondInterface;
+    public function millisecond(): MillisecondInterface;
     public function format(FormatInterface $format): string;
     public function changeTimezone(TimezoneInterface $zone): self;
     public function timezone(): TimezoneInterface;
---
+++ src/TimeContinuum/SecondInterface.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\TimeContinuum\TimeContinuum;
+
+interface SecondInterface
+{
+    public function toInt(): int;
+    public function __toString(): string;
+}
