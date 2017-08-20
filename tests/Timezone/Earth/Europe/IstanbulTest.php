240cba7003ff7e7376afb136a58e84aea2295cce
Baptiste Langlade <baptouuuu@gmail.com>
2017-08-20 15:04:00 +0200

    since 2016 istanbul timezone is utc+0300 even with dst in effect

--- tests/Timezone/Earth/Europe/IstanbulTest.php
+++ tests/Timezone/Earth/Europe/IstanbulTest.php
@@ -16,14 +16,8 @@ class IstanbulTest extends TestCase
         $zone = new Istanbul;
 
         $this->assertInstanceOf(TimezoneInterface::class, $zone);
-        if ($zone->daylightSavingTimeApplied()) {
-            $this->assertSame(3, $zone->hours());
-            $this->assertSame(0, $zone->minutes());
-            $this->assertSame('+03:00', (string) $zone);
-        } else {
-            $this->assertSame(2, $zone->hours());
-            $this->assertSame(0, $zone->minutes());
-            $this->assertSame('+02:00', (string) $zone);
-        }
+        $this->assertSame(3, $zone->hours());
+        $this->assertSame(0, $zone->minutes());
+        $this->assertSame('+03:00', (string) $zone);
     }
 }
