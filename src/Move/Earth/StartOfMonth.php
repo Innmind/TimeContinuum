cc8f926950de719a000d000c910e9b629369fbeb
Baptiste Langlade <baptouuuu@gmail.com>
2018-04-28 15:11:57 +0200

    add helper to move a point in time to the start of the day

--- src/Move/Earth/StartOfMonth.php
+++ src/Move/Earth/StartOfMonth.php
@@ -5,23 +5,15 @@ namespace Innmind\TimeContinuum\Move\Earth;
 
 use Innmind\TimeContinuum\{
     PointInTimeInterface,
-    Period\Earth\Day,
-    Period\Earth\Hour,
-    Period\Earth\Minute,
-    Period\Earth\Second,
-    Period\Earth\Millisecond
+    Period\Earth\Day
 };
 
 final class StartOfMonth
 {
     public function __invoke(PointInTimeInterface $point): PointInTimeInterface
     {
-        return $point->goBack(
-            (new Day($point->day()->toInt() - 1))
-                ->add(new Hour($point->hour()->toInt()))
-                ->add(new Minute($point->minute()->toInt()))
-                ->add(new Second(max($point->second()->toInt() - 1, 0)))
-                ->add(new Millisecond($point->millisecond()->toInt()))
+        return (new StartOfDay)($point)->goBack(
+            new Day($point->day()->toInt() - 1)
         );
     }
 }
