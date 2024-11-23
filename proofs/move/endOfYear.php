08f83faed011a49b61c0d388d03dcb5d4eba2bec
Baptiste Langlade <baptouuuu@gmail.com>
2024-11-23 15:11:10 +0100

    add move proofs

---
+++ proofs/move/endOfYear.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+
+use Innmind\TimeContinuum\{
+    Clock,
+    Move\EndOfYear,
+};
+use Fixtures\Innmind\TimeContinuum\PointInTime;
+use Innmind\BlackBox\Set;
+
+return static function() {
+    yield proof(
+        'End of month',
+        given(Set\Either::any(
+            PointInTime::any(),
+            Set\Call::of(static fn() => Clock::live()->now()),
+        )),
+        static function($assert, $point) {
+            $endOfYear = (new EndOfYear)($point);
+
+            $assert->same(
+                $point->year()->toInt(),
+                $endOfYear->year()->toInt(),
+            );
+            $assert->same(
+                12,
+                $endOfYear->month()->ofYear()->toInt(),
+            );
+            $assert->same(
+                31,
+                $endOfYear->day()->ofMonth(),
+            );
+            $assert->same(
+                23,
+                $endOfYear->hour()->toInt(),
+            );
+            $assert->same(
+                59,
+                $endOfYear->minute()->toInt(),
+            );
+            $assert->same(
+                59,
+                $endOfYear->second()->toInt(),
+            );
+            $assert->same(
+                999,
+                $endOfYear->millisecond()->toInt(),
+            );
+        },
+    );
+};
---
+++ proofs/move/startOfMonth.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+
+use Innmind\TimeContinuum\{
+    Clock,
+    Move\StartOfMonth,
+};
+use Fixtures\Innmind\TimeContinuum\PointInTime;
+use Innmind\BlackBox\Set;
+
+return static function() {
+    yield proof(
+        'End of day',
+        given(Set\Either::any(
+            PointInTime::any(),
+            Set\Call::of(static fn() => Clock::live()->now()),
+        )),
+        static function($assert, $point) {
+            $startOfMonth = (new StartOfMonth)($point);
+
+            $assert->same(
+                $point->year()->toInt(),
+                $startOfMonth->year()->toInt(),
+            );
+            $assert->same(
+                $point->month()->ofYear(),
+                $startOfMonth->month()->ofYear(),
+            );
+            $assert->same(
+                1,
+                $startOfMonth->day()->ofMonth(),
+            );
+            $assert->same(
+                0,
+                $startOfMonth->hour()->toInt(),
+            );
+            $assert->same(
+                0,
+                $startOfMonth->minute()->toInt(),
+            );
+            $assert->same(
+                0,
+                $startOfMonth->second()->toInt(),
+            );
+            $assert->same(
+                0,
+                $startOfMonth->millisecond()->toInt(),
+            );
+        },
+    );
+};
