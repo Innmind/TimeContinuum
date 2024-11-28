eda5ef85ad4208e8e2e430f98ba489d85f45d95e
Baptiste Langlade <baptouuuu@gmail.com>
2024-11-28 19:38:16 +0100

    test negative cases

--- proofs/elapsedPeriod.php
+++ proofs/elapsedPeriod.php
@@ -43,6 +43,7 @@ return static function() {
             );
         },
     );
+
     yield proof(
         'High resolution elapsed period within same second',
         given(
@@ -103,6 +104,45 @@ return static function() {
                     ->longerThan(
                         Period::microsecond(0)->asElapsedPeriod(),
                     ),
+            );
+        },
+    );
+
+    yield proof(
+        'Negative elapsed periods throws',
+        given(
+            Fixtures\PointInTime::any(),
+            Set\Integers::above(1),
+        ),
+        static function($assert, $start, $microsecond) {
+            $assert->throws(
+                static fn() => $start
+                    ->goBack(Period::microsecond($microsecond))
+                    ->elapsedSince($start),
+            );
+        },
+    );
+
+    yield proof(
+        'Negative high resolution elapsed periods throws',
+        given(
+            Set\Integers::above(0),
+            Set\Integers::above(0),
+            Set\Integers::between(0, 999_999_999),
+            Set\Integers::between(0, 999_999_999),
+        )->filter(static fn($start, $end) => $end > $start),
+        static function(
+            $assert,
+            $startSeconds,
+            $endSeconds,
+            $startNanoseconds,
+            $endNanoseconds,
+        ) {
+            $start = HighResolution::of($startSeconds, $startNanoseconds);
+            $end = HighResolution::of($endSeconds, $endNanoseconds);
+
+            $assert->throws(
+                static fn() => $start->elapsedSince($end),
             );
         },
     );
